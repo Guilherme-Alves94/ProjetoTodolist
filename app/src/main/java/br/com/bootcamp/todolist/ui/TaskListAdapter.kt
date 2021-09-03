257e163d5eff0f39792b7573cb3fd1fc2de5715a
G2A94 <g.gerbas@gmail.com>
2021-09-03 14:57:34 -0300

    implementação da função de criar e deletar tarefas

--- app/src/main/java/br/com/bootcamp/todolist/ui/TaskListAdapter.kt
+++ app/src/main/java/br/com/bootcamp/todolist/ui/TaskListAdapter.kt
@@ -2,14 +2,19 @@ package br.com.bootcamp.todolist.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import br.com.bootcamp.todolist.R
 import br.com.bootcamp.todolist.databinding.ItemTaskBinding
 import br.com.bootcamp.todolist.model.Task
 
 
 class TaskListAdapter : ListAdapter<Task, TaskListAdapter.TaskViewHolder>(DiffCallBack()){
+
+    var listenerEdit : (Task) -> Unit = {}
+    var listenerDelete : (Task) -> Unit = {}
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
@@ -22,11 +27,29 @@ class TaskListAdapter : ListAdapter<Task, TaskListAdapter.TaskViewHolder>(DiffCa
         holder.bind(getItem(position))
     }
 
-    class TaskViewHolder(private val binding: ItemTaskBinding
+    inner class TaskViewHolder(private val binding: ItemTaskBinding
     ) : RecyclerView.ViewHolder(binding.root){
         fun bind(item: Task) {
             binding.tvTitle.text = item.title
             binding.tvDate.text = "${item.date} ${item.hour}"
+            binding.ivMore.setOnClickListener {
+                showPopup(item)
+            }
+        }
+
+        private fun showPopup(item: Task) {
+            val ivMore = binding.ivMore
+            val popupMenu = PopupMenu(ivMore.context, ivMore)
+            popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener {
+                when(it.itemId){
+                    R.id.action_edit -> listenerEdit(item)
+                    R.id.action_delete -> listenerDelete(item)
+                }
+                return@setOnMenuItemClickListener true
+            }
+            popupMenu.show()
+
         }
 
     }
